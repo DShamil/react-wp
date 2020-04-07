6340aa6dc315338374d409a8d0c61c882617696b
Shamil <shamil.samaraweera@siprea.com>
2020-04-07 21:39:27 +0530

    Updates

---
+++ precache-manifest.a7da2c8dc266e80a3ad2c798149b5f7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d05421c256fdc6fe36e90c13cd33f3",
+    "url": "/react-wp/index.html"
+  },
+  {
+    "revision": "7b6d78dc2ad87d7f33f9",
+    "url": "/react-wp/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d6bd00d0e8b9a463bdeb",
+    "url": "/react-wp/static/css/main.bf94a6bd.chunk.css"
+  },
+  {
+    "revision": "7b6d78dc2ad87d7f33f9",
+    "url": "/react-wp/static/js/2.506d9b10.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-wp/static/js/2.506d9b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6bd00d0e8b9a463bdeb",
+    "url": "/react-wp/static/js/main.6fe3cf0b.chunk.js"
+  },
+  {
+    "revision": "4e03964893e9d3006d36",
+    "url": "/react-wp/static/js/runtime-main.178cfbac.js"
+  }
+]);
